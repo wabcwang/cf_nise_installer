c20526c4d199bf7cb74d16e83b702cc7b31e8566
wangc <wangc@dtdream.com>
2015-07-24 17:28:13 +0800

    按需启动进程，不直接启动所有的进程

--- scripts/start.sh
+++ scripts/start.sh
@@ -6,16 +6,17 @@ cd "$path"/../
 sudo /var/vcap/bosh/bin/monit
 sleep 5
 
-for process in \
-    postgres \
-    nats
-do
-    sudo /var/vcap/bosh/bin/monit start $process
-    sleep 30
-done;
+if [ ""!="$( find /var/vcap/jobs -name postgres)" ]; then
+    sudo /var/vcap/bosh/bin/monit start postgres
+	sleep 20
+fi
+
+if [ ""!= "$( find /var/vcap/jobs -name nats)" ]; then
+    sudo /var/vcap/bosh/bin/monit start nats
+	sleep 20
+fi
 
 sudo /var/vcap/bosh/bin/monit start all
-
 
 echo "Waiting for all processes to start"
 for ((i=0; i < 120; i++)); do
