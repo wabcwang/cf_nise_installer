84324483d59809d093c6e5db3c2bbea190396c75
wangc <wangc@dtdream.com>
2015-07-27 15:39:46 +0800

    修改配置文件的路径

--- scripts/generate_deploy_manifest.sh
+++ scripts/generate_deploy_manifest.sh
@@ -5,11 +5,11 @@ NISE_IP_ADDRESS=${NISE_IP_ADDRESS:-`ip addr | grep 'inet .*global' | cut -f 6 -d
 
 sed "s/192.168.10.10/${NISE_IP_ADDRESS}/g" manifests/template.yml > manifests/deploy.yml
 
-DB_IP=$(grep  "^ *DB_IP" cf.conf |awk -F "=" '$1{print $2}')
+DB_IP=$(grep  "^ *DB_IP" ./scripts/cf.conf |awk -F "=" '$1{print $2}')
 if [ "${DB_IP}" != "" ]; then
     sed "s/DB_ADDRESS/${DB_IP}/g" manifests/template.yml > manifests/deploy.yml
 fi
-NISE_DOMAIN=$(grep  "^ *DOMAIN" cf.conf |awk -F "=" '$1{print $2}')
+NISE_DOMAIN=$(grep  "^ *DOMAIN" ./scripts/cf.conf |awk -F "=" '$1{print $2}')
 if [ "${NISE_DOMAIN}" != "" ]; then
     if (! sed --version 1>/dev/null 2>&1); then
         # not a GNU sed
@@ -19,7 +19,7 @@ if [ "${NISE_DOMAIN}" != "" ]; then
     fi
 fi
 
-NISE_DOMAIN=$(grep  "^ *PASSWD" cf.conf |awk -F "=" '$1{print $2}')
+NISE_DOMAIN=$(grep  "^ *PASSWD" ./scripts/cf.conf |awk -F "=" '$1{print $2}')
 if [ "${NISE_PASSWORD}" != "" ]; then
     if (! sed --version 1>/dev/null 2>&1); then
         # not a GNU sed
